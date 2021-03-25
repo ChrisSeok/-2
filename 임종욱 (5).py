548224ac103b34151b3d9b3c7524a1fb67c3350e
lim-jonguk <81340019+lim-jonguk@users.noreply.github.com>
2021-03-26 08:31:08 +0900

    Delete 임종욱 (5).py

--- 임종욱 (5).py
+++
@@ -1,9 +0,0 @@
-# 4번 C111152 Jong Uk Lim
-
-Thou = int(input('1000원권 개수: '))*1000
-Goods= int(input('물건값: ' ))
-change = Thou - Goods
-hund5 = change//500
-hund1 = (change- hund5*500)//100
-print('500원 동전의 개수: ',hund5)
-print('100원 동전의 개수: ',hund1)
